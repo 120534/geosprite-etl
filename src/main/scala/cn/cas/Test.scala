13baa79edd620fd5ad1b7f715e0ed704a12832c7
wanghanlin <lhkwhl@qq.com>
2018-08-31 20:42:22 +0800

    new pipeline demo

---
+++ src/main/scala/cn/cas/Test.scala
@@ -0,0 +1,14 @@
+package cn.cas
+
+import geotrellis.raster.{Raster, Tile}
+
+object Test {
+
+  import geotrellis.proj4.CRS
+  import geotrellis.raster.io.geotiff.SinglebandGeoTiff
+
+    def main(args: Array[String]): Unit = {
+
+
+  }
+}
